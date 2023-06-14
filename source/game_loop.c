83b239ee46183727bde34d71d7354a842727b432
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-06-14 23:54:42 +0200

    add comments to game_loop fucntion

--- source/game_loop.c
+++ source/game_loop.c
@@ -1,5 +1,14 @@
 #include "game.h"
 
+/**
+ * game_loop - this will be the main game loop which
+ * will run the game and check if the game 
+ * is still opening or closed
+ * @textured: integer represeting if the game should have 
+ * image textures or not
+ * @maze: an array of numbers represting walls on the maze
+*/
+
 void game_loop(int textured, int *maze)
 {
 	while (!quit())
